50fd39d8c59f690b2973aeff94ae5e155c24c741
Connor Slade <turtleslade@gmail.com>
2021-04-13 22:08:06 -0400

    🎨 Make Color in DebugPrint Optinal

--- src/SchoolPasswordCrack.py
+++ src/SchoolPasswordCrack.py
@@ -8,13 +8,16 @@ from datetime import datetime
 ############ VARS ############
 configFile = 'config/config.confnose'
 DEBUG = True
+COLOR = True
+
+ColorCodes = {'black': '30', 'red': '31', 'yellow': '33', 'green': '32', 'blue': '34',
+                  'cyan': '36', 'magenta': '35', 'white': '37', 'gray': '90', 'reset': '0'}
 
 ######### FUNCTIONS #########
 
 
 def colored(text, color):
-    ColorCodes = {'black': '30', 'red': '31', 'yellow': '33', 'green': '32', 'blue': '34',
-                  'cyan': '36', 'magenta': '35', 'white': '37', 'gray': '90', 'reset': '0'}
+    if not COLOR: return text
     return '\033[' + ColorCodes[str(color).lower()] + 'm' + str(text) + "\033[0m"
 
 
